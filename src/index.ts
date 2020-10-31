2d4c5fa1da09d2389ac1fba91fcf484391a7a010
Anderson <48699535+andeer0601@users.noreply.github.com>
2020-10-31 18:51:34 -0300

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+﻿require('dotenv').config({
+    path: process.env.NODE_ENV === 'test' ? ".env.test" : ".env"
+})
+
+import express from 'express'
+
+import './database/connection'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello"
+    })
+})
+
+app.listen( process.env.PORT || 3333 )
